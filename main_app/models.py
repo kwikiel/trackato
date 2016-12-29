7c0efc2ef4178f1bd9a91933ad62e6d51d5a726e
Kacper Wikieł <k.wikiel@gmail.com>
2016-12-29 08:53:38 +0100

    Model for treasure

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Treasure(models.Model):
+
+		name = models.CharField(max_length=100)
+		value = models.DecimalField(max_digits=10, decimal_places=2)
+
